534d633234d4d1b8dfae5088991da30881e1d4b1
dragondreams <catsrule2007@gmail.com>
2015-08-14 20:49:12 -0700

    initiating a web130 project

---
+++ js/bxslider.js
@@ -0,0 +1,10 @@
+<!-- jQuery library (served from Google) -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+
+<!-- bxSlider Javascript file -->
+<script src="/js/jquery.bxslider.min.js"></script>
+
+$('.bxslider').bxSlider({
+  mode: 'fade',
+  captions: true
+});
